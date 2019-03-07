1ad41a98bddc73e8e00c9594674045d71f294d95
Tim Riffe <tim.riffe@gmail.com>
2019-03-07 18:23:34 +0100

    .

--- GlobalViolence/R/DataPrep_Graduate.R
+++ GlobalViolence/R/DataPrep_Graduate.R
@@ -100,30 +100,34 @@ GBD.mono.ms <- function(.SD, omega=110,lambda=.1,mort="h"){
 	
 	nlast  <- omega - max(x) + 1
 	offset <- y / m
-	off1   <- splitMono(offset, AgeInt = age2int(Age = x, OAvalue = nlast))
+	off1   <- splitMono(offset, AgeInt = age2int(Age = x, OAvalue = nlast))[-1]
+	off1[off1<0] <- 0
 	m5     <- rep(m, times = age2int(Age = x, OAvalue = nlast))
 
 	w <- rep(1,110)
-	w[off1[-1] == 0] <- 0
+	ind <- off1 == 0 | is.na(off1) | is.infinite(off1)
+	w[ind] <- 0
 	
-	d      <- m5[-1] * off1[-1]
+	d      <- m5[-1] * off1
+	d[ind] <- 0
 	if (floor(sum(d)) < 10){
 		return(m5)
 	} 
-	if (sum(d) < 1000){
+	if (sum(d) < 100){
 		fac <- 100
 	} else {
 		fac <- 1
 	}
 	d      <- d * fac
+	
 	mod    <- suppressWarnings(Mort1Dsmooth(
 			    x = 1:110, 
 			    y = d, 
-			    offset = log(off1[-1]), 
+			    offset = log(off1), 
 			    w = w, 
 			    lambda = lambda, 
 			    method = 3))
-	mx     <- c(m0, exp(predict(mod, newdata = 1:110)) / fac)
+	mx     <- c(m0, exp(mod$logmortality) / fac)
 	mx
 }
 
@@ -142,7 +146,11 @@ GBD.chunk <- function(.SD,omega=110){
 					M=M,Mh=Mh,Mw=Mw))
 }
 
-GBD.chunk(sdl[[1]])
+
+.SD <- sdl[[124]]
+GBD.chunk(.SD)
+
+sdL  <- mclapply(sdl[sample(1:length(sdl),size=1000,replace=FALSE)], GBD.chunk, mc.cores = 3)
 dir.create(file.path("Data","Single","GBD"), showWarnings = FALSE, recursive = TRUE)
 
 gbd.folder <- file.path("Data","Grouped","GBD")
@@ -153,8 +161,9 @@ variants <- c("low","mid","upp")
 for (i in 1:length(variants)){
 	GBDi <- local(get(load(file.path("Data","Grouped","GBD",paste0("GBD",variants[i],".Rdata")))))
 	sdl  <- split(GBDi,list(GBDi$location,GBDi$Sex,GBDi$year))
-	sdl  <- mclapply(sdl, GBD.chunk, mc.cores = 3)
-	GBDi <- rbindlist(sdl)
+	sdL <- list()
+	sdL  <- mclapply(sdl, GBD.chunk, mc.cores = 3)
+	GBDi <- rbindlist(sdL)
 	rm(sdl);gc()
 	save(GBDi,file=file.path("Data","Single","GBD",paste0("GBD",variants[i],".Rdata")))
 	rm(GBDi);gc()
