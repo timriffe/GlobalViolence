92fe9a7cc22bfa036001070315df1def26591eb7
vdilego <vdilego@gmail.com>
2020-02-11 16:06:42 +0100

    iso code suggestions and age variable disappearence

--- GlobalViolence/R/Uncertainty.R
+++ GlobalViolence/R/Uncertainty.R
@@ -23,7 +23,7 @@ for (i in 1:length(variants)){
 	
 	# choose an explicit closeout file
   # don´t know why but for me "sex" was saved without capital "s"
-  # also now you add and "a" before all causes of death..
+  # also now you add and "a" before all causes of death (check sequence from DataPrep)..
   
 	GBDi <- readRDS(here("GlobalViolence","Data","Closeout","GBD",
 									paste0("GBD",variants[i],"_ggompertz_65_90_65.rds")))
@@ -40,36 +40,40 @@ for (i in 1:length(variants)){
 	rm(GBDi);gc()
 }
 
-for (i in 1:length(variants)){
   
   # choose an explicit closeout file
-  GBDi <- readRDS(here("GlobalViolence","Data","Closeout","GBD",
-                       paste0("GBD",variants[i],"_ggompertz_65_90_65.rds"))) 
-  GBDi[,.(sdx=mx2sd(Ma),
-          sdx_no_h=mx2sd(Ma-Mh),
-          sdx_no_hw=mx2sd(Ma-Mh-Mw),
-          edx=mx2edagger(Ma),
-          edx_no_h=mx2edagger(Ma-Mh),
-          edx_no_hw=mx2edagger(Ma-Mh-Mw),
-          ex=mx2ex(Ma),
-          ex_no_h=mx2ex(Ma-Mh),
-          ex_no_hw=mx2ex(Ma-Mh-Mw)),.(location,year,sex)] %>% 
-  saveRDS( file = here("GlobalViolence","Data","Results","GBD",paste0("GBD",variants[i],".rds")))
-  rm(GBDi);gc()
-}
+  
+  # something happens here age disappears in the final file - I did not get this;
+  # Is it the same but now you offer a pipeline+data.table approach?
+  # I just kept the same data.table sintax from before and it seemed better.
+  # please check what you think. Maybe this second step is an additional thing
+  # and I am missing something...
+  
+  for (i in 1:length(variants)){
+    
+    # choose an explicit closeout file
+    GBDi <- readRDS(here("GlobalViolence","Data","Closeout","GBD",
+                         paste0("GBD",variants[i],"_ggompertz_65_90_65.rds"))) 
+    GBDi[,.(sdx=mx2sd(Ma),
+            sdx_no_h=mx2sd(Ma-Mh),
+            sdx_no_hw=mx2sd(Ma-Mh-Mw),
+            edx=mx2edagger(Ma),
+            edx_no_h=mx2edagger(Ma-Mh),
+            edx_no_hw=mx2edagger(Ma-Mh-Mw),
+            ex=mx2ex(Ma),
+            ex_no_h=mx2ex(Ma-Mh),
+            ex_no_hw=mx2ex(Ma-Mh-Mw)),.(location,year,sex)] %>% 
+      saveRDS( file = here("GlobalViolence","Data","Results","GBD",paste0("GBD",variants[i],".rds")))
+    rm(GBDi);gc()
+  }
+
 # oops we need to recuperate ISO codes
 # ISO Codes for mapping. This should be earlier in processing, move at some point.
-ISO           <- read.csv(file.path("Data","Inputs","GBD","GBD_ISO3.csv"),
-		stringsAsFactors = FALSE)
-recvec        <- ISO[, 2]
-names(recvec) <- ISO[, 1]
 
-for (i in 1:length(variants)){
-	GBDi      <- local(get(load(file.path("Data","Results","GBD",paste0("GBD",variants[i],".Rdata")))))
-	GBDi$ISO3 <- recvec[GBDi$location]
-	save(GBDi, file = file.path("Data","Results","GBD",paste0("GBD",variants[i],".Rdata")))
-	rm(GBDi);gc()
-}
+# I think we should either add them using the countrycode package I mentioned in the 
+# DataPrep or we should just add the ISO codes when mapping. I think we can just leave it 
+# for the Maps code, since it is not really important before that and this just seems a bit
+# messier, so I would just skip this next part altogether..what do you think?
 
 
 
